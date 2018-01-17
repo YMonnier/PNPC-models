51b0b34586009c5ce02a12c2651836a446231e2b
Stephen Bellanger <stephenbellanger@gmail.com>
2018-01-17 17:33:43 +0100

    Add comments

--- src/main/java/fr/pnpc/project/models/dao/QueryParameter.java
+++ src/main/java/fr/pnpc/project/models/dao/QueryParameter.java
@@ -8,19 +8,50 @@ import java.util.Map;
  */
 public class QueryParameter {
 
+    /**
+     * Parameters query.
+     */
     private Map parameters = null;
 
+    /**
+     * Constructor
+     *
+     * @param name The string key name.
+     * @param value The value of parameter.
+     */
     private QueryParameter(String name,Object value){
         this.parameters = new HashMap();
         this.parameters.put(name, value);
     }
+
+    /**
+     * Set a new parameter.
+     *
+     * @param name The string key name.
+     * @param value The value of parameter.
+     * @return
+     */
     public static QueryParameter with(String name, Object value){
         return new QueryParameter(name, value);
     }
+
+    /**
+     * Set a parameter to the current query.
+     *
+     * @param name The string key name.
+     * @param value The value of parameter.
+     * @return
+     */
     public QueryParameter and(String name,Object value){
         this.parameters.put(name, value);
         return this;
     }
+
+    /**
+     * Return a Map with all parameters.
+     *
+     * @return Single Map of parameters.
+     */
     public Map parameters(){
         return this.parameters;
     }

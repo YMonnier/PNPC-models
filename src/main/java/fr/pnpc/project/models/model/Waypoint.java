2fb80ebcbfacd96bd61c7a59802250fba0505256
Stephen Bellanger <stephenbellanger@gmail.com>
2018-01-18 16:20:17 +0100

    Add comments

--- src/main/java/fr/pnpc/project/models/model/Waypoint.java
+++ src/main/java/fr/pnpc/project/models/model/Waypoint.java
@@ -15,27 +15,51 @@ import java.util.Collection;
 @Data
 public class Waypoint implements Serializable {
 
+    /**
+     * Waypoint identifier.
+     */
     @Id
     @GeneratedValue
     private long id;
 
+    /**
+     * Beacon identifier of current waypoint.
+     * Identifier for identify beacon.
+     */
     private String beaconId;
 
+    /**
+     * Latitude of current waypoint.
+     */
     @NotNull(message = "Latitude should not be null.")
     @Latitude
     private Double latitude;
 
+    /**
+     * Longitude of current waypoint.
+     */
     @NotNull(message = "Longitude should not be null.")
     @Longitude
     private Double longitude;
 
+    /**
+     * The list of all the waypoints of the current waypoint.
+     */
     @OneToMany(mappedBy = "waypoint", cascade = CascadeType.PERSIST)
     private Collection<Passage> passages;
 
+    /**
+     * Default constructor
+     */
     public Waypoint() {
         this.passages = new ArrayList<>();
     }
 
+    /**
+     * Constructor
+     * To create a builder. @see `Builder`
+     * @param builder builder Builder object.
+     */
     private Waypoint(Builder builder) {
         this.latitude = builder.latitude;
         this.longitude = builder.longitude;

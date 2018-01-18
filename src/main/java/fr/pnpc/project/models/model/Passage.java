78552c688fc6abf243f95fd9141af1919d186f36
Stephen Bellanger <stephenbellanger@gmail.com>
2018-01-18 16:08:08 +0100

    Add comments

--- src/main/java/fr/pnpc/project/models/model/Passage.java
+++ src/main/java/fr/pnpc/project/models/model/Passage.java
@@ -20,21 +20,49 @@ import java.io.Serializable;
 @Data
 public class Passage implements Serializable {
 
+    /**
+     * Name of the named query to find all passages
+     * with user identifier passed in parameter.
+     */
     public final static String FIND_BY_USER_ID = "FIND_PASSAGES_BY_USER_ID";
+
+    /**
+     * Name of the named query to find a passage
+     * with his identifier passed in parameter.
+      */
     public final static String FIND_BY_ID = "FIND_PASSAGES_BY_ID";
 
+    /**
+     * Passage identifier.
+     */
     @Id
     @GeneratedValue
     private long id;
 
+    /**
+     * The user who owns the current passage*
+     */
     @NotNull(message = "User object should not be null.")
     private User user;
 
+    /**
+     * The waypoint who owns the current passage
+     */
     @NotNull(message = "Waypoint object should not be null.")
     private Waypoint waypoint;
 
+    /**
+     * Default constructor
+     * Is require when the constructor is instanciate during
+     * the injection
+     */
     public Passage(){}
 
+    /**
+     * Constructor
+     * @param user User object.
+     * @param waypoint Waypoint object.
+     */
     public Passage(User user, Waypoint waypoint) {
         this.user = user;
         this.waypoint = waypoint;

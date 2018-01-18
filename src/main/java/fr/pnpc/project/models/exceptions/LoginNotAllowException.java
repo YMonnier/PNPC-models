4023adac28539097afbebfabeb1776fa98e53aab
Stephen Bellanger <stephenbellanger@gmail.com>
2018-01-18 16:00:12 +0100

    Add comments

--- src/main/java/fr/pnpc/project/models/exceptions/LoginNotAllowException.java
+++ src/main/java/fr/pnpc/project/models/exceptions/LoginNotAllowException.java
@@ -1,5 +1,9 @@
 package fr.pnpc.project.models.exceptions;
 
+/**
+ * This exception is raised when a user wants
+ * to log on to an application and their nickname or password is invalid.
+ */
 public class LoginNotAllowException extends Exception {
     public LoginNotAllowException() {
         super("Bad credentials.");
--- src/main/java/fr/pnpc/project/models/exceptions/NotFoundException.java
+++ src/main/java/fr/pnpc/project/models/exceptions/NotFoundException.java
@@ -1,5 +1,9 @@
 package fr.pnpc.project.models.exceptions;
 
+/**
+ * This exception is raised when an entity does not exist
+ * or is not present in the database.
+ */
 public class NotFoundException extends Exception {
     public NotFoundException(String typeObject) {
         super(typeObject + " does not exist.");
